34f1bfb777993264bf49115e16642fe31e18ef9a
BT_1N3_32 <BT_1N3_32@BT_1N3_32.bktech.local>
2018-01-16 11:24:07 -0500

    hi

---
+++ SymonSays Game/src/mainMenuAndStartScreen/MainMenuScreen.java
@@ -0,0 +1,20 @@
+package mainMenuAndStartScreen;
+
+import guiTeacher.GUIApplication;
+
+public class MainMenuScreen extends GUIApplication{
+
+	public MainMenuScreen(int width, int length) {
+		super(width,length);
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+	@Override
+	public void initScreen() {
+		
+	}
+
+}
---
+++ SymonSays Game/src/mainMenuAndStartScreen/StartScreen.java
@@ -0,0 +1,22 @@
+package mainMenuAndStartScreen;
+
+import guiTeacher.GUIApplication;
+
+public class StartScreen extends GUIApplication{
+
+	public StartScreen(int width, int length) {
+		super(width,length);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void initScreen() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
