580fc2a5fb2fae929af44ec5cb99b2a788949c3e
BT_1N3_27 <BT_1N3_27@BT_1N3_27.bktech.local>
2018-01-16 11:03:25 -0500

    added images folder

--- SymonSays Game/src/creditsCarsonAmanat/Credits.java
+++ SymonSays Game/src/creditsCarsonAmanat/Credits.java
@@ -1,25 +1,24 @@
 package creditsCarsonAmanat;
 
 import guiTeacher.GUIApplication;
-import guiTeacher.userInterfaces.Screen;
 
 public class Credits extends GUIApplication{
 
+	private static final long serialVersionUID = 1L;
 	public static Credits sample;
 	public static CreditsScreen credits;
 	
 	public Credits(int width, int height) {
 		super(width, height);
-		setVisible(true);	}
+		setVisible(true);	
+	}
 
 	public static void main(String[] args){
 		sample = new Credits(1000, 1000);
 		Thread go = new Thread(sample);
 		go.start();
-		
-		
 	}
-	@Override
+	
 	public void initScreen() {
 		credits = new CreditsScreen(getWidth(), getHeight());
 		setScreen(credits);		
--- SymonSays Game/src/creditsCarsonAmanat/CreditsScreen.java
+++ SymonSays Game/src/creditsCarsonAmanat/CreditsScreen.java
@@ -2,13 +2,18 @@ package creditsCarsonAmanat;
 
 import java.util.List;
 
+import guiTeacher.components.TextLabel;
 import guiTeacher.interfaces.Visible;
 import guiTeacher.userInterfaces.ClickableScreen;
 
 public class CreditsScreen extends ClickableScreen implements Runnable{
-	
+
+	private static final long serialVersionUID = 1L;
+
 	public CreditsScreen(int width, int height) {
 		super(width, height);
+		Thread app = new Thread(this);
+		app.start();
 	}
 	
 	public void run() {
@@ -16,7 +21,8 @@ public class CreditsScreen extends ClickableScreen implements Runnable{
 	}
 
 	public void initAllObjects(List<Visible> viewObjects) {
-		
+		TextLabel title = new TextLabel(100, 100, 200, 100, "Credits");
+		viewObjects.add(title);
 	}
 
 }
