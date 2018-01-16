c4eda9f62e58599feba1567093f05c981a5aae80
BT_1N3_32 <BT_1N3_32@BT_1N3_32.bktech.local>
2018-01-16 11:28:22 -0500

    started screen

---
+++ SymonSays Game/src/bossSelect/SelectScreen.java
@@ -0,0 +1,28 @@
+package bossSelect;
+
+import guiTeacher.GUIApplication;
+
+public class SelectScreen extends GUIApplication {
+	
+	private String name;
+	private String link;
+	private int level;
+	private int levelRec; 
+
+	public SelectScreen(int width, int height) {
+		super(width, height);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void initScreen() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
