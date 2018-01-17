3378ee948f1f545a949cded6b287bc64be87ac86
BT_1N3_25 <BT_1N3_25@BT_1N3_25.bktech.local>
2018-01-17 11:34:33 -0500

    needa finish at homee

--- SymonSays Game/src/settingsCarsonAmanat/Settings.java
+++ SymonSays Game/src/settingsCarsonAmanat/Settings.java
@@ -1,18 +1,22 @@
 package settingsCarsonAmanat;
 
+import java.io.File;
+
+import creditsCarsonAmanat.Credits;
 import guiTeacher.GUIApplication;
 
 public class Settings extends GUIApplication{
 
 	public static Settings sample;
 	public static SettingsScreen symon;
+	public static Credits credits;
 	
 	public Settings(int width, int height) {
 		super(width, height);
 		setVisible(true);	}
 
 	public static void main(String[] args){
-		sample = new Settings(1000, 1000);
+		sample = new Settings(500, 350);
 		Thread go = new Thread(sample);
 		go.start();
 		
