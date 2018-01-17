40f1326c14188e0723b42c378c448801e442bb11
BT_1N3_24 <BT_1N3_24@BT_1N3_24.bktech.local>
2018-01-17 11:32:30 -0500

    shopmenu frontend

--- SymonSays Game/src/shopmenu/ShopScreen.java
+++ SymonSays Game/src/shopmenu/ShopScreen.java
@@ -1,14 +1,21 @@
 package shopmenu;
 
+import java.awt.Font;
+import java.io.File;
 import java.util.List;
 
 import guiTeacher.components.Action;
 import guiTeacher.components.Button;
+import guiTeacher.components.Graphic;
+import guiTeacher.components.StyledComponent;
 import guiTeacher.components.TextArea;
+import guiTeacher.components.TextField;
+import guiTeacher.components.TextLabel;
 import guiTeacher.interfaces.Visible;
 import guiTeacher.userInterfaces.ClickableScreen;
+import guiTeacher.userInterfaces.FullFunctionScreen;
 
-public class ShopScreen extends ClickableScreen {
+public class ShopScreen extends FullFunctionScreen {
 
 	public ShopScreen(int width, int height) {
 		super(width, height);
@@ -18,17 +25,20 @@ public class ShopScreen extends ClickableScreen {
 	@Override
 	public void initAllObjects(List<Visible> viewObjects) {
 		
-		TextArea x = new TextArea(150,100,50,50,"MERCHANT");
-		Button buy = new Button(100,100,50,50,"Buy",new Action() {
+		setCustomFont();
+		viewObjects.add(new Graphic(0, 0, getWidth(),getHeight(),"shopUpgradeResources/bgrnd.jpg"));
+		TextArea title = new TextArea(300,50,300,50,"MERCHANT");
+		viewObjects.add(title);
+		Button buy = new Button(50,450,100,50,"Buy",new Action() {
 			
 			@Override
 			public void act() {
-			
+			 System.out.println("Buy");
 			}
 		});
 		viewObjects.add(buy);
 		
-		Button sell = new Button(100,100,50,50,"Sell",new Action() {
+		Button sell = new Button(150,450,100,50,"Sell",new Action() {
 			
 			@Override
 			public void act() {
@@ -38,7 +48,7 @@ public class ShopScreen extends ClickableScreen {
 	
 		viewObjects.add(sell);
 		
-		Button upgrade = new Button(100,100,50,50,"Upgrade",new Action() {
+		Button upgrade = new Button(250,450,200,50,"Upgrade",new Action() {
 			
 			@Override
 			public void act() {
@@ -47,7 +57,7 @@ public class ShopScreen extends ClickableScreen {
 		});
 		
 		viewObjects.add(upgrade);
-		Button back = new Button(100,100,50,50,"Back",new Action() {
+		Button back = new Button(450,450,100,50,"Back",new Action() {
 			
 			@Override
 			public void act() {
@@ -56,8 +66,21 @@ public class ShopScreen extends ClickableScreen {
 		});
 		
 		viewObjects.add(back);
+
 		
 		}
+	private void setCustomFont(){
+		
+		try {
+			File fontFile = new File("shopUpgradeResources//Orbitron-Black.ttf");
+			Font font = Font.createFont(Font.TRUETYPE_FONT, fontFile).deriveFont(30f);
+			StyledComponent.setBaseFont(font);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 		
 }
 	
