5cf5ef07a23db8589e552284ed5c40e1fe11271a
David <David@Davids-Pc1>
2018-01-19 02:32:56 -0500

    friday stuff

---
+++ SymonSays Game/src/mainMenuAndStartScreen/GameStarter.java
@@ -0,0 +1,23 @@
+package mainMenuAndStartScreen;
+
+import guiTeacher.GUIApplication;
+
+public class GameStarter extends GUIApplication{
+
+	public GameStarter(int width, int height) {
+		super(width, height);
+		setVisible(true);
+	}
+
+	public static StartScreen start;
+	public static MainMenuScreen mainMenu;
+	
+	public static void main(String[] args) {
+		
+	}
+
+	public void initScreen() {
+		
+	}
+
+}
---
+++ SymonSays Game/src/mainMenuAndStartScreen/MainMenuScreen.java
@@ -0,0 +1,5 @@
+package mainMenuAndStartScreen;
+
+public class MainMenuScreen {
+
+}
