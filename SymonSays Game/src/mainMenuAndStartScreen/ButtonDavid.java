1902c2aa0cfb4a6173e1f98f75a391034062f402
David <David@Davids-Pc1>
2018-01-19 02:39:45 -0500

    custom buttons

---
+++ SymonSays Game/src/mainMenuAndStartScreen/ButtonDavid.java
@@ -0,0 +1,13 @@
+package mainMenuAndStartScreen;
+
+import guiTeacher.components.Action;
+import guiTeacher.components.Button;
+
+public class ButtonDavid extends Button{
+
+	public ButtonDavid(int x, int y, int w, int h, String text, Action action) {
+		super(x, y, w, h, text, action);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ SymonSays Game/src/mainMenuAndStartScreen/ButtonInterfaceDavid.java
@@ -0,0 +1,5 @@
+package mainMenuAndStartScreen;
+
+public interface ButtonInterfaceDavid {
+
+}
