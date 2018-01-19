2b0cd5e300cc9518ebe20ba144fa8bb95af6ed75
BT_1N3_31 <BT_1N3_31@BT_1N3_31.bktech.local>
2018-01-19 11:13:01 -0500

    a

--- SymonSays Game/src/shopUpgrade/UpgradeScreen.java
+++ SymonSays Game/src/shopUpgrade/UpgradeScreen.java
@@ -18,16 +18,21 @@ public class UpgradeScreen extends FullFunctionScreen {
 	private Button upgradeButton;
 	private TextArea upgradeDescription;
 	private TextLabel upgradeName;
+	private TextLabel title;
 	private int upgradePoints;
 
 	public UpgradeScreen(int width, int height) {
 		super(width, height);
+		setVisible(true);
 		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public void initAllObjects(List<Visible> viewObjects) {
-		backButton.setTextColor(Color.BLACK);
+		title = new TextLabel(400,50,100,50,"Upgrades");
+		title.setTextColor(Color.WHITE);
+		viewObjects.add(title);
+		viewObjects.add(new Graphic(0, 0, getWidth(),getHeight(),"shopUpgradeResources/bgrnd.jpg"));
 		backButton = new Button(0,20,100,50,"Back", new Action() {
 
 			@Override
