0c9ca353e88d0062c1c03c30b4832434d0817b60
BT_1N3_30 <BT_1N3_30@BT_1N3_30.bktech.local>
2018-01-31 11:11:25 -0500

    push

--- SymonSays Game/src/shopmenu/DavidSell.java
+++ SymonSays Game/src/shopmenu/DavidSell.java
@@ -22,21 +22,21 @@ public class DavidSell extends FullFunctionScreen {
 
 	@Override
 	public void initAllObjects(List<Visible> viewObjects) {
-		title.setTextColor(Color.WHITE);
+		//title.setTextColor(Color.WHITE);
 		title =  new TextArea(600,50,300,50,"MERCHANT");
 		TextArea desc1 = new TextArea(600,150,400,100,"USED TO INCREASE DEFENSE");
-		TextArea quant1 = new TextArea(500,160,300,100,"x10");
-		TextArea desc2 = new TextArea(600,250,400,100,"HEALS PLAYER FOR 100HP");
-		TextArea quant2 = new TextArea(500,260,300,100,"x10");
-		TextArea desc3 = new TextArea(600,350,450,100,"INCREASES DODGE RATE");
-		TextArea quant3 = new TextArea(500,360,300,100,"x10");
-		TextArea desc4 = new TextArea(600,450,300,100,"INCREASES ATK BY 50Pts");
-		TextArea quant4 = new TextArea(500,460,300,100,"x10");
+		TextArea quant1 = new TextArea(500,150,300,100,"x10");
+		TextArea desc2 = new TextArea(600,275,400,100,"HEALS PLAYER FOR 100HP");
+		TextArea quant2 = new TextArea(500,275,300,100,"x10");
+		TextArea desc3 = new TextArea(600,400,450,100,"INCREASES DODGE RATE");
+		TextArea quant3 = new TextArea(500,400,300,100,"x10");
+		TextArea desc4 = new TextArea(600,525,300,100,"INCREASES ATK BY 50Pts");
+		TextArea quant4 = new TextArea(500,525,300,100,"x10");
 		viewObjects.add(new Graphic(0, 0, getWidth()*2,getHeight()*2,"shopUpgradeResources/bgrnd.jpg"));
 		viewObjects.add(new Graphic(420, 150, 75,75,"shopUpgradeResources/test.png"));
 		viewObjects.add(new Graphic(420, 250, 75,75,"shopUpgradeResources/health.png"));
-		viewObjects.add(new Graphic(420, 350, 75,75,"shopUpgradeResources/speed.png"));
-		viewObjects.add(new Graphic(420, 450, 75,75,"shopUpgradeResources/strength.png"));
+		viewObjects.add(new Graphic(420, 400, 75,75,"shopUpgradeResources/speed.png"));
+		viewObjects.add(new Graphic(420, 500, 75,75,"shopUpgradeResources/strength.png"));
 		viewObjects.add(title);
 		viewObjects.add(desc1);
 		viewObjects.add(quant1);
@@ -60,25 +60,25 @@ public class DavidSell extends FullFunctionScreen {
 	
 		viewObjects.add(back);
 		
-		Button buyH = new Button(900, 170, 100, 100, "Sell", new Action() {
+		Button buyH = new Button(900, 175, 100, 100, "Sell", new Action() {
 			public void act() {
 				
 			}
 		});
 		viewObjects.add(buyH);
-		Button buyS = new Button(900, 270, 100, 100, "Sell", new Action() {
+		Button buyS = new Button(900, 300, 100, 100, "Sell", new Action() {
 			public void act() {
 				
 			}
 		});
 		viewObjects.add(buyS);
-		Button buyR = new Button(900, 370, 100, 100, "Sell", new Action() {
+		Button buyR = new Button(900, 425, 100, 100, "Sell", new Action() {
 			public void act() {
 			
 			}
 		});
 		viewObjects.add(buyR);
-		Button buyHM = new Button(900, 470, 100, 100, "Sell", new Action() {
+		Button buyHM = new Button(900, 525, 100, 100, "Sell", new Action() {
 			public void act() {
 				
 			}
--- SymonSays Game/src/shopmenu/RickyBuy.java
+++ SymonSays Game/src/shopmenu/RickyBuy.java
@@ -5,16 +5,13 @@ import java.util.List;
 
 import guiTeacher.components.Action;
 import guiTeacher.components.Button;
+import guiTeacher.components.Graphic;
+import guiTeacher.components.TextArea;
 import guiTeacher.components.TextLabel;
 import guiTeacher.interfaces.Visible;
 import guiTeacher.userInterfaces.ClickableScreen;
 
 public class RickyBuy extends ClickableScreen {
-
-	public RickyBuy(int width, int height) {
-		super(width, height);
-		setVisible(true);
-	}
 
 	public static final int COST = 200;
 	private int quantityOwned;
@@ -25,19 +22,39 @@ public class RickyBuy extends ClickableScreen {
 	private TextLabel desc;
 	private TextLabel youown;
 	
-
+	public RickyBuy(int width, int height) {
+		super(width, height);
+	}
+	
 	@Override
 	public void initAllObjects(List<Visible> viewObjects) {
-		back = new Button(0,100,20,50,"Back",new Action() {
+		Graphic background = new Graphic(0, 0, getWidth() * 2, getHeight() * 2,"shopUpgradeResources/bgrnd.jpg");
+		viewObjects.add(background);
+		back = new Button(0,20,125,50,"Back",Color.BLUE, new Action() {
 			
 			@Override
 			public void act() {
-				//goes to MainMenu Screen
+				ShopMain.s1.setScreen(ShopMain.s2);
 			}
 		});
-		back.setForeground(Color.BLACK);
-		back.update();
+		//back.setForeground(Color.BLACK);
+		//back.update();
 		viewObjects.add(back);
+		TextArea title =  new TextArea(600,50,300,50,"MERCHANT");
+		viewObjects.add(title);
+		TextArea desc1 = new TextArea(600,150,400,100,"USED TO INCREASE DEFENSE");
+		TextArea desc2 = new TextArea(600,275,400,100,"HEALS PLAYER FOR 100HP");
+		TextArea desc3 = new TextArea(600,400,450,100,"INCREASES DODGE RATE");
+		TextArea desc4 = new TextArea(600,525,300,100,"INCREASES ATK BY 50Pts");
+		for(int i = 0; i < 4; i++) {
+			TextArea multiplier = new TextArea(500,150 + i*125,300,100,"x10");
+			viewObjects.add(multiplier);
+		}
+		viewObjects.add(desc1);
+		viewObjects.add(desc2);
+		viewObjects.add(desc3);
+		viewObjects.add(desc4);
+
 	}
 
 
