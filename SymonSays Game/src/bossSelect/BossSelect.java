19280a55faffdb0afc4e6dfec67290e4ee82e587
BT_1N3_32 <BT_1N3_32@BT_1N3_32.bktech.local>
2018-02-05 11:21:26 -0500

    ficked back and more edit

--- SymonSays Game/src/bossSelect/BossSelect.java
+++ SymonSays Game/src/bossSelect/BossSelect.java
@@ -1,8 +1,13 @@
 package bossSelect;
 
 import guiTeacher.GUIApplication;
+import guiTeacher.userInterfaces.Screen;
+import mainMenuAndStartScreen.MainMenuScreen;
 
 public class BossSelect extends GUIApplication {
+	
+	public static Screen backScreen;
+	public static BossSelect boss;
 
 	public BossSelect(int width, int height) {
 		super(width, height);
@@ -12,13 +17,14 @@ public class BossSelect extends GUIApplication {
 	@Override
 	public void initScreen() {
 		SelectScreen s = new SelectScreen(getWidth(), getHeight());
+		backScreen = new MainMenuScreen(getWidth(), getHeight());
 		setScreen(s);
 
 	}
 
 	public static void main(String[] args) {
-		BossSelect sample = new BossSelect(1400, 780);
-		Thread go = new Thread(sample);
+		boss = new BossSelect(1400, 780);
+		Thread go = new Thread(boss);
 		go.start();
 	}
 
--- SymonSays Game/src/bossSelect/SelectScreen.java
+++ SymonSays Game/src/bossSelect/SelectScreen.java
@@ -10,11 +10,14 @@ import java.util.List;
 
 import guiTeacher.components.Action;
 import guiTeacher.components.Button;
+import guiTeacher.components.Component;
 import guiTeacher.components.Graphic;
 import guiTeacher.components.StyledComponent;
 import guiTeacher.components.TextArea;
 import guiTeacher.interfaces.Visible;
 import guiTeacher.userInterfaces.FullFunctionScreen;
+import mainMenuAndStartScreen.ButtonDavid;
+import mainMenuAndStartScreen.GameStarter;
 
 public class SelectScreen extends FullFunctionScreen {
 	
@@ -35,82 +38,90 @@ public class SelectScreen extends FullFunctionScreen {
 
 	
 	public void initAllObjects(List<Visible> viewObjects) {
-		initFont();
+		initGothicFont(24f);
 		initValues();
 		StyledComponent.setButtonOutline(true);
 		StyledComponent.setActiveBorderColor(Color.white);
-		BackStartButton boss1Button = new BackStartButton(20, y, wh, wh, "Boss 1", new Action() {
+		Button boss1Button = new Button(20, y, wh, wh, "Boss 1", new Action() {
 			public void act() {
 				name = "Boss 1";
 				link = "resources/Dragon.jpg";
 				level = 5;
 				levelRec = 1;
-				txt.setText("Name: " + name + "\nLevel: " + level + "\nLevel Rec: " + levelRec);
+				txt.setText("Name: " + name + "\n\nLevel: " + level + "\n\nLevel Rec: " + levelRec);
 				background = new Graphic(0, 0, getWidth(), getHeight(), link);
 				viewObjects.add(0, background);
 				viewObjects.remove(1);
 			}
 		});
-		Button boss2Button = new BackStartButton(140, y, wh, wh, "Boss 2", new Action() {
+		Button boss2Button = new Button(140, y, wh, wh, "Boss 2", new Action() {
 			public void act() {
 				name = "Boss 2";
 				link = "resources/Xeno.jpg";
 				level = 5;
 				levelRec = 1;
-				txt.setText("Name: " + name + "\nLevel: " + level + "\nLevel Rec: " + levelRec);
+				txt.setText("Name: " + name + "\n\nLevel: " + level + "\n\nLevel Rec: " + levelRec);
 				background = new Graphic(0, 0, getWidth(), getHeight(), link);
 				viewObjects.add(0, background);
 				viewObjects.remove(1);
 			}
 		});
-		Button boss3Button = new BackStartButton(260, y, wh, wh, "Boss 3", new Action() {
+		Button boss3Button = new Button(260, y, wh, wh, "Boss 3", new Action() {
 			public void act() {
 				name = "Boss 3";
 				link = "resources/Ghaul.jpg";
 				level = 5;
 				levelRec = 1;
-				txt.setText("Name: " + name + "\nLevel: " + level + "\nLevel Rec: " + levelRec);
+				txt.setText("Name: " + name + "\n\nLevel: " + level + "\n\nLevel Rec: " + levelRec);
 				background = new Graphic(0, 0, getWidth(), getHeight(), link);
 				viewObjects.add(0, background);
 				viewObjects.remove(1);
 			}
 		});
-		Button boss4Button = new BackStartButton(380, y, wh, wh, "Boss 4", new Action() {
+		Button boss4Button = new Button(380, y, wh, wh, "Boss 4", new Action() {
 			public void act() {
 				name = "Boss 4";
 				link = "";
 				level = 5;
 				levelRec = 1;
-				txt.setText("Name: " + name + "\nLevel: " + level + "\nLevel Rec: " + levelRec);
+				txt.setText("Name: " + name + "\n\nLevel: " + level + "\n\nLevel Rec: " + levelRec);
 				background = new Graphic(0, 0, getWidth(), getHeight(), link);
 				viewObjects.add(0, background);
 				viewObjects.remove(1);
 			}
 		});
-		Button boss5Button = new BackStartButton(500, y, wh, wh, "Boss 5", new Action() {
+		Button boss5Button = new Button(500, y, wh, wh, "Boss 5", new Action() {
 			public void act() {
 				name = "Boss 5";
 				link = "";
 				level = 5;
 				levelRec = 1;
-				txt.setText("Name: " + name + "\nLevel: " + level + "\nLevel Rec: " + levelRec);
+				txt.setText("Name: " + name + "\n\nLevel: " + level + "\n\nLevel Rec: " + levelRec);
 				background = new Graphic(0, 0, getWidth(), getHeight(), link);
 				viewObjects.add(0, background);
 				viewObjects.remove(1);
 			}
 		});
-		Button boss6Button = new BackStartButton(620, y, wh, wh, "Boss 6", new Action() {
+		Button boss6Button = new Button(620, y, wh, wh, "Boss 6", new Action() {
 			public void act() {
 				name = "Boss 6";
 				link = "";
 				level = 5;
 				levelRec = 1;
-				txt.setText("Name: " + name + "\nLevel: " + level + "\nLevel Rec: " + levelRec);
+				txt.setText("Name: " + name + "\n\nLevel: " + level + "\n\nLevel Rec: " + levelRec);
 				background = new Graphic(0, 0, getWidth(), getHeight(), link);
 				viewObjects.add(0, background);
 				viewObjects.remove(1);
 			}
 		});
+		initGothicFont(36f);
+		ButtonDavid backButton = new ButtonDavid(50,680,100,Color.lightGray,"Back",new Action() {
+			
+			public void act() {
+				GameStarter.start.setScreen(GameStarter.mainMenuScreen);
+			}
+		});
+		initGothicFont(30f);
 		background = new Graphic(0, 0, 1400, 780, link);
 		txt = new TextArea(20, 250, 300, 300,"Name: " + name + "\n\nLevel: " + level + "\n\nLevel Rec: " + levelRec);
 		viewObjects.add(background);
@@ -121,6 +132,7 @@ public class SelectScreen extends FullFunctionScreen {
 		viewObjects.add(boss4Button);
 		viewObjects.add(boss5Button);
 		viewObjects.add(boss6Button);
+		viewObjects.add(backButton);
 		viewObjects.add(txt);
 		boss1Button.setForeground(Color.white);
 		boss2Button.setForeground(Color.white);
@@ -128,17 +140,19 @@ public class SelectScreen extends FullFunctionScreen {
 		boss4Button.setForeground(Color.white);
 		boss5Button.setForeground(Color.white);
 		boss6Button.setForeground(Color.white);
+		txt.setCustomTextColor(Color.white);
 	}
-	
-	public void initFont() {
+
+
+	public void initGothicFont(float f) {
 		try {
-		File fontFile = new File("resources/orbitron-bold.otf");
+		File fontFile = new File("resources/bankgothic_medium_bt.ttf");
 		Font font = Font.createFont(Font.TRUETYPE_FONT, fontFile);
-		Font baseFont=font.deriveFont(24f);
+		Font baseFont=font.deriveFont(f);
 		StyledComponent.setBaseFont(baseFont);
 	} catch (Exception e) {
 		e.printStackTrace();
-	}
+		}
 	}
 
 
