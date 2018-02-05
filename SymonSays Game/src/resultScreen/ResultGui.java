621a6d6a400a9fa7fb9b6a8b9444844a5f5458ff
BT_1N3_26 <BT_1N3_26@BT_1N3_26.bktech.local>
2018-02-05 11:34:23 -0500

    make main class

--- SymonSays Game/src/resultScreen/ResultGui.java
+++ SymonSays Game/src/resultScreen/ResultGui.java
@@ -12,19 +12,19 @@ public class ResultGui extends GUIApplication {
 		setVisible(true);
 		
 	}
-	
-	@Override
-	public void initScreen() {
-		screen = new ResultScreen(getWidth(), getHeight());
-		setScreen(screen);
-		
-	}
 	public static void main(String[] args) {
 		main = new ResultGui(1400, 780);
 		screen = new ResultScreen(1400, 780);
 		Thread go = new Thread(main);
 		go.start();
 	}
+	@Override
+	public void initScreen() {
+		screen = new ResultScreen(getWidth(), getHeight());
+		setScreen(screen);
+		
+	}
+	
 	
 	
 }
--- SymonSays Game/src/resultScreen/ResultScreen.java
+++ SymonSays Game/src/resultScreen/ResultScreen.java
@@ -12,16 +12,17 @@ public class ResultScreen extends FullFunctionScreen {
 	
 	public ResultScreen(int width, int height) {
 		super(width, height);
+		
 		// TODO Auto-generated constructor stub
 	}
 	public void defineValues() {
-		//bossName = ;
-		//bossLVL =  ;
+		bossName = sisi.bossName();
+		bossLVL =  ;
 	}
 	@Override
 	public void initAllObjects(List<Visible> viewObjects) {
-		Graphic back = new Graphic(0 , 0, getWidth()/2, getHeight(), "resources/drag.jpeg");
-		viewObjects.add(back);
+		Graphic backGround = new Graphic(0 , 0, getWidth(), getHeight(), "resources/black.jpg");
+		viewObjects.add(backGround);
 		
 	}
 
