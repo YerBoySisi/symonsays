180cc48dca7192fa85548bc7d55da2a0d96d2d07
BT_1N3_31 <BT_1N3_31@BT_1N3_31.bktech.local>
2018-02-07 11:39:36 -0500

    Working on another set of upgrades

--- SymonSays Game/src/shopmenu/UpgradeScreen.java
+++ SymonSays Game/src/shopmenu/UpgradeScreen.java
@@ -23,15 +23,25 @@ public class UpgradeScreen extends FullFunctionScreen {
 	private TextLabel upgradeName1;
 	private TextLabel upgradeName2;
 	private TextLabel upgradeName3;
+	private TextLabel upgradeName4;
+	private TextLabel upgradeName5;
+	private TextLabel upgradeName6;
 	private TextLabel title;
 	private TextLabel pointsDisplay;
 	private int upgradePoints;
 	private ArrayList<Button> buttons1;
 	private ArrayList<Button> buttons2;
-	private Upgrade riposte = new Upgrade(5, 50, "% chance to attack again");
-	private Upgrade regeneration = new Upgrade(50, 40, "HP healed each turn");
-	private Upgrade agility = new Upgrade(2, 30, "% chance to dodge an attack");
-	private int[] costs;
+	private Upgrade riposte;
+	private Upgrade regeneration;
+	private Upgrade agility;
+	private Upgrade defense;
+	private Upgrade attack;
+	private Upgrade magic;
+	private Upgrade[] upgrades1;
+	private Upgrade[] upgrades2;
+	private int[] costs1;
+	private int[] costs2;
+	private boolean front = true;
 
 	public UpgradeScreen(int width, int height) {
 		super(width, height);
@@ -63,7 +73,7 @@ public class UpgradeScreen extends FullFunctionScreen {
 		viewObjects.add(icon3);
 		setBankGothic();
 		addBackgrounds(viewObjects);
-		addDescriptions(viewObjects);
+		addDescriptions1(viewObjects);
 		setUpgradePoints(300);
 		pointsDisplay = new TextLabel(1200, 20, 200, 50, "UP: " + getUpgradePoints());
 		pointsDisplay.setCustomTextColor(Color.CYAN);
@@ -78,7 +88,9 @@ public class UpgradeScreen extends FullFunctionScreen {
 		});
 		viewObjects.add(backButton);
 		addbuttons1();
-		setButtonActions();
+		setButton1Actions();
+		addbuttons2();
+		setButton2Actions();
 		for(Button b: buttons1) {
 			viewObjects.add(b);
 		}
@@ -86,7 +98,11 @@ public class UpgradeScreen extends FullFunctionScreen {
 
 			@Override
 			public void act() {
-				ShopMain.s1.setScreen(ShopMain.s2);
+				addBackgrounds(viewObjects);
+				addDescriptions2(viewObjects);
+				for(Button b: buttons2) {
+					viewObjects.add(b);
+				}
 			}
 			
 		});
@@ -133,14 +149,36 @@ public class UpgradeScreen extends FullFunctionScreen {
 		}
 	}
 	
-	public void setButtonActions() {
+	public void addbuttons2() {
+		buttons2 = new ArrayList<Button>();
+		for(int i = 1; i <= 3; i++) {
+			for(int j = 1; j <= 3; j++) {
+				Button b = new Button(200 + 450 * (i - 1), 100 + 175 * j, 200, 50, "Upgrade", Color.GREEN, new Action() {
+
+					@Override
+					public void act() {
+						
+					}
+					
+				});
+				if(j != 1) {
+					b.setEnabled(false);
+					b.setBackground(Color.RED);
+					b.update();
+				}
+				buttons2.add(b);
+			}
+		}
+	}
+	
+	public void setButton1Actions() {
 		for(Button b: buttons1) {
 			b.setAction(new Action() {
 
 				@Override
 				public void act() {
-					if(getUpgradePoints() >= costs[buttons1.indexOf(b)]) {
-						setUpgradePoints(getUpgradePoints() - costs[buttons1.indexOf(b)]);
+					if(getUpgradePoints() >= costs1[buttons1.indexOf(b)]) {
+						setUpgradePoints(getUpgradePoints() - costs1[buttons1.indexOf(b)]);
 						pointsDisplay.setText("UP: " + getUpgradePoints());
 						b.setEnabled(false);
 						b.setBackground(Color.WHITE);
@@ -158,31 +196,85 @@ public class UpgradeScreen extends FullFunctionScreen {
 		}
 	}
 	
-	public void addDescriptions(List<Visible> viewObjects) {
-		costs = new int[9];
+	public void setButton2Actions() {
+		for(Button b: buttons2) {
+			b.setAction(new Action() {
+
+				@Override
+				public void act() {
+					if(getUpgradePoints() >= costs2[buttons2.indexOf(b)]) {
+						setUpgradePoints(getUpgradePoints() - costs2[buttons2.indexOf(b)]);
+						pointsDisplay.setText("UP: " + getUpgradePoints());
+						b.setEnabled(false);
+						b.setBackground(Color.WHITE);
+						b.setText("Upgraded");
+						b.update();
+						if((buttons2.indexOf(b) + 1) % 3 != 0) {
+							Button nextButton = buttons2.get(buttons2.indexOf(b) + 1);
+							nextButton.setEnabled(true);
+							nextButton.setBackground(Color.GREEN);
+							nextButton.update();
+						}
+					}
+				}
+			});
+		}
+	}
+	
+	public void addDescriptions1(List<Visible> viewObjects) {
+		createUpgradeList1();
+		costs1 = new int[9];
 		for(int i = 0; i < 3; i++) {
 			for(int j = 1; j <= 3; j++) {
-				String desc = "";
-				int cost = 50 - 10 * i;
-				if(i == 0) {
-					desc = "+" + j * 5 + "% chance to attack again";
-				}
-				if(i == 1) {
-					desc = "Heal " + j * 50 + "HP every turn";
-				}
-				if(i == 2) {
-					desc = "+" + j * 2 + "% chance to dodge an attack";
-				}
-				TextArea upgradeDescription = new TextArea(50 + 450 * i, 175 * j, 350, 300, desc);
+				String desc = upgrades1[i].getDesc();
+				int cost = upgrades1[i].getCost();
+				TextArea upgradeDescription = new TextArea(50 + 450 * i, 175 * j, 350, 300, "+" + upgrades1[i].getMultiple() * j + desc);
 				viewObjects.add(upgradeDescription);
 				TextLabel upgradeCost = new TextLabel(50 + 450 * i, 100 + 175 * j, 300, 50, cost * j + "UP");
 				upgradeCost.setCustomTextColor(Color.CYAN);
 				viewObjects.add(upgradeCost);
-				costs[i * 3 + (j - 1)] = cost * j;
+				costs1[i * 3 + (j - 1)] = cost * j;
 			}
 		}
 	}
-
+	
+	public void createUpgradeList1() {
+		riposte = new Upgrade(5, 50, "% chance to attack again");
+		regeneration = new Upgrade(50, 40, "HP healed each turn");
+		agility = new Upgrade(2, 30, "% chance to dodge an attack");
+		upgrades1 = new Upgrade[3];
+		upgrades1[0] = riposte;
+		upgrades1[1] = regeneration;
+		upgrades1[2] = agility;
+	}
+	
+	public void addDescriptions2(List<Visible> viewObjects) {
+		createUpgradeList2();
+		costs2 = new int[9];
+		for(int i = 0; i < 3; i++) {
+			for(int j = 1; j <= 3; j++) {
+				String desc = upgrades2[i].getDesc();
+				int cost = upgrades2[i].getCost();
+				TextArea upgradeDescription = new TextArea(50 + 450 * i, 175 * j, 350, 300, "+" + upgrades2[i].getMultiple() * j + desc);
+				viewObjects.add(upgradeDescription);
+				TextLabel upgradeCost = new TextLabel(50 + 450 * i, 100 + 175 * j, 300, 50, cost * j + "UP");
+				upgradeCost.setCustomTextColor(Color.CYAN);
+				viewObjects.add(upgradeCost);
+				costs2[i * 3 + (j - 1)] = cost * j;
+			}
+		}
+	}
+	
+	public void createUpgradeList2() {
+		attack = new Upgrade(20, 50, "POW");
+		defense = new Upgrade(10, 40, "DEF");
+		magic = new Upgrade(20, 30, "MP");
+		upgrades2 = new Upgrade[3];
+		upgrades2[0] = attack;
+		upgrades2[1] = defense;
+		upgrades2[2] = magic;
+	}
+	
 	public int getUpgradePoints() {
 		return upgradePoints;
 	}
