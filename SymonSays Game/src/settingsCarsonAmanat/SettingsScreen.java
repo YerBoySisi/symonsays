e352f90edbc60e62f39b97822ae076c820b4e478
BT_1N3_25 <BT_1N3_25@BT_1N3_25.bktech.local>
2018-02-07 11:41:00 -0500

    stuff

--- SymonSays Game/src/settingsCarsonAmanat/SettingsScreen.java
+++ SymonSays Game/src/settingsCarsonAmanat/SettingsScreen.java
@@ -2,8 +2,10 @@ package settingsCarsonAmanat;
 
 import java.util.ArrayList;
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.MouseEvent;
+import java.io.File;
 import java.util.List;
 
 import javax.swing.ImageIcon;
@@ -13,6 +15,7 @@ import creditsCarsonAmanat.Credits;
 import guiTeacher.components.Action;
 import guiTeacher.components.Button;
 import guiTeacher.components.Graphic;
+import guiTeacher.components.StyledComponent;
 import guiTeacher.components.TextArea;
 import guiTeacher.components.TextLabel;
 import guiTeacher.interfaces.Visible;
@@ -37,8 +40,8 @@ public class SettingsScreen extends FullFunctionScreen{
 
 	@Override
 	public void initAllObjects(List<Visible> viewObjects) {
+
 		viewObjects.add(new Graphic(0, 0, getWidth(),getHeight(),"resources/earth.jpg"));
-		TextArea title = new TextArea(getWidth()/2-50, getHeight() - 300, 200, 200, "Settings");
 		Button creditt = new Button(100,250,100,50,"Credits",Color.GRAY,new Action() {
 
 			@Override
@@ -53,15 +56,27 @@ public class SettingsScreen extends FullFunctionScreen{
 			public void act() {
 			}
 		});
+		try {
+			File fontFile = new File("resources/bankgothic_medium_bt.ttf");
+			Font font = Font.createFont(Font.TRUETYPE_FONT, fontFile);
+			Font baseFont=font.deriveFont(36f);
+			StyledComponent.setBaseFont(baseFont);
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+
+		TextLabel title = new TextLabel(getWidth()/2-100,getHeight()-750,300,200,"Settings");
+		title.setCustomTextColor(Color.lightGray);
 		volumeSlider = new Button(100, 100, 15, 15, "",Color.WHITE, null);
 		volumeRect = new Button(80, 80, 200, 100, "", Color.BLACK, new Action() {
-			
+
 			@Override
 			public void act() {
-					
-				
+
+
 			}
-			
+
 		});
 		ButtonAmanat settingGear = new ButtonAmanat(400, 400, 120, 120, gearIcon);
 		viewObjects.add(title);
@@ -71,19 +86,22 @@ public class SettingsScreen extends FullFunctionScreen{
 		viewObjects.add(settingGear);
 
 	}
-	
+
 	public static ImageIcon createImageIcon(String path) {
-	    return new ImageIcon("resources/gear.png");
+		return new ImageIcon("resources/gear.png");
 	}
-	
+
 	public void mouseDragged(MouseEvent m) {
 		super.mouseDragged(m);
-		if(volumeSlider.getX()>99) 
-		volumeSlider.setX(m.getX());
-		if(volumeSlider.getX()<100)
-			volumeSlider.setX(100);
-		if(getY()==100) 
-		volumeSlider.setY(m.getY());
+		super.mouseClicked(m);
+		if(m.getY() >95 && m.getY()<150) {
+			if(volumeSlider.getX()>99) 
+				volumeSlider.setX(m.getX());
+			if(volumeSlider.getX()<100)
+				volumeSlider.setX(100);
+			if(getY()==100) 
+				volumeSlider.setY(m.getY());
+		}
 	}
 
 }
