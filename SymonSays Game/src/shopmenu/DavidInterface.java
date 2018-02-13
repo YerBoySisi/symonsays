be9a4b9556ff99d3fec5176af6965256d2459a40
BT_1N3_27 <BT_1N3_27@BT_1N3_27.bktech.local>
2018-02-13 10:37:19 -0500

    c

---
+++ SymonSays Game/src/shopmenu/DavidInterface.java
@@ -0,0 +1,12 @@
+package shopmenu;
+
+import java.util.ArrayList;
+
+import inv.Items;
+
+public class DavidInterface {
+
+	void countOccurences(ArrayList<Items> list, Items x);
+	 void removeItem(ArrayList<Items> list,Items x);
+	
+}
